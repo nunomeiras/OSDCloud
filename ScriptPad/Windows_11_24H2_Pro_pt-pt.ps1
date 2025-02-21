de0f1349a7c9b739ca2e09b90a6911e3f52259db
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2025-02-21 15:17:01 +0000

    Update Windows_11_24H2_Pro_pt-pt.ps1

--- ScriptPad/Windows_11_24H2_Pro_pt-pt.ps1
+++ ScriptPad/Windows_11_24H2_Pro_pt-pt.ps1
@@ -90,7 +90,7 @@ New-ItemProperty -LiteralPath "HKLM:\Software\Policies\Microsoft\Internet Explor
 New-Item "C:\MSUpdates\LCU" -ItemType Directory -Force
 
 Write-Host "Downloading Latest Cumulative Update for Windows 11 24H2 - 02 Fevereiro, 2025"
-curl.exe -L -o "C:\MSupdates\LCU\Windows11-24H2-LCU.msu" "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d94b06df-34e8-42be-9e62-afe448c65da8/public/windows11.0-kb5051987-x64_199ed7806a74fe78e3b0ef4f2073760000f71972.msu"
+curl.exe -L -o "C:\MSupdates\LCU\Windows11-24H2-LCU.msu" "C:\MSupdates\LCU\Windows11-24H2-LCU.msu" "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d8b7f92b-bd35-4b4c-96e5-46ce984b31e0/public/windows11.0-kb5043080-x64_953449672073f8fb99badb4cc6d5d7849b9c83e8.msu"
 
 # Use old unattended method instead of Provisioning ppkg to install drivers
 Set-OSDCloudUnattendSpecialize

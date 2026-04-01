439560e6a0c83629e1282d61d74589714feecfc9
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2026-04-01 23:19:31 +0100

    Update Start-Deployment.ps1

--- ScriptPad/Start-Deployment.ps1
+++ ScriptPad/Start-Deployment.ps1
@@ -1,25 +1,55 @@
-# Instalar módulos necessários
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
-Install-Module OSDCloud -Force
 
-Import-Module OSD
-Import-Module OSDCloud
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
 
-# Configuração idioma PT-PT
-$Global:OSDCloud = @{
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
     OSVersion = "Windows 11"
     OSBuild = "25H2"
     OSEdition = "Enterprise"
-    OSLanguage = "pt-pt"
+    OSLanguage = "pt-PT"
     OSLicense = "Volume"
+    ZTI = $true
+    Firmware = $false
 }
+Start-OSDCloud @Params
 
-# URL do autounattend no GitHub
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+try {
+    if (-not (Test-Path -Path "C:\Windows\Panther")) {
+        New-Item -Path "C:\Windows\Panther" -ItemType Directory -Force
+    }
+
+powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
 $UnattendURL = "https://raw.githubusercontent.com/nunomeiras/OSDCloud/autounattend.xml"
+$UnattendPath = "C:\Windows\Panther\Unattend.xml"
+Invoke-WebRequest -Uri $UnattendURL -OutFile $UnattendPath
+
+
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
+
+
+
 
 # Download autounattend
-$UnattendPath = "C:\OSDCloud\autounattend.xml"
-Invoke-WebRequest -Uri $UnattendURL -OutFile $UnattendPath
+
 
 # Aplicar deployment
 Start-OSDCloud -UnattendPath $UnattendPath

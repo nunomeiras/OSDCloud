52a11bcc35eb340e058e5e6bbdc9e441ed999263
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2026-04-01 19:35:05 +0100

    Update W11_pt_PT_CMP.ps1

--- ScriptPad/W11_pt_PT_CMP.ps1
+++ ScriptPad/W11_pt_PT_CMP.ps1
@@ -1,1 +1,22 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
 
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "25H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "pt-pt"
+    OSLicense = "Volume"
+    ZTI = $true
+    Firmware = $false
+}
+Start-OSDCloud @Params

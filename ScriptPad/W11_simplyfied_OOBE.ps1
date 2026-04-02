86fe3de87fcda51b677425edd53cf879a0a14eb7
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2026-04-03 00:19:22 +0100

    Update W11_simplyfied_OOBE.ps1

--- ScriptPad/W11_simplyfied_OOBE.ps1
+++ ScriptPad/W11_simplyfied_OOBE.ps1
@@ -1,11 +1,5 @@
 #================================================
 #   [PreOS] Update Module
-#================================================
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
-
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
